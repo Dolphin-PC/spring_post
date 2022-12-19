714f555a587c6968cba54a901ba6a5943f132b90
jake <pcx474@gmail.com>
2022-12-19 18:56:07 +0900

    initial commit

---
+++ src/test/java/com/dolphin/spring_post/SpringPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolphin.spring_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
